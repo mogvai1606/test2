e84e759c9ded67255690c660faadf23de15a82b6
Alexandr <mogvai6062@gmail.com>
2020-08-27 17:46:32 +0700

    "lesson3"

---
+++ test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const str = "test";
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello dude";
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseFloat(test));
